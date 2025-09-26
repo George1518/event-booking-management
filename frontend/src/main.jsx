180d8f65dde3499a3095a7ba562815332c6cb625
george1518 <guganraj.me@gmail.com>
2025-09-26 16:13:40 +0530

    Configure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 
+const basename = import.meta.env.BASE_URL || '/event-booking-management/';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </AuthProvider>
