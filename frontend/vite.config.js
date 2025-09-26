00b0a4bdef4f400a78308e256d0fc5f626cfb696
george1518 <guganraj.me@gmail.com>
2025-09-26 15:42:57 +0530

    Configure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'event-booking-management', // Change this to your actual repo name
+  base: '/event-booking-management/', 
   server: {
     proxy: {
       '/api': {
